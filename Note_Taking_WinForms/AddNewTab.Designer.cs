df4b920bfc4dfb18d8935ade5570e607a39d7946
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-07-24 11:48:04 +0300

    New Form

---
+++ Note_Taking_WinForms/AddNewTab.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Note_Taking_WinForms
+{
+    partial class AddNewTab
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.NameTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.AutoSize = true;
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddButton.Location = new System.Drawing.Point(71, 102);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(114, 35);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // NameTB
+            // 
+            this.NameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameTB.Location = new System.Drawing.Point(52, 50);
+            this.NameTB.Name = "NameTB";
+            this.NameTB.Size = new System.Drawing.Size(159, 30);
+            this.NameTB.TabIndex = 1;
+            // 
+            // AddNewTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 163);
+            this.Controls.Add(this.NameTB);
+            this.Controls.Add(this.AddButton);
+            this.Name = "AddNewTab";
+            this.Text = "Название";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox NameTB;
+    }
+}
---
+++ Note_Taking_WinForms/AddNewTab.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_Taking_WinForms
+{
+    public partial class AddNewTab : Form
+    {
+        public AddNewTab()
+        {
+            InitializeComponent();
+        }
+
+        public string NewName { get; set; }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            NewName = NameTB.Text;
+
+            if (NewName == "")
+                MessageBox.Show("");
+            else
+            {
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+    }
+}
